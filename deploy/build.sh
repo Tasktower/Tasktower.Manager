1734f797f0ba866872cbe7476b9d1ca9a1db77a9
Oren Ben-Meir <orenbm11@gmail.com>
2021-04-18 22:41:47 -0400

    illegal option & script refractor

--- deploy/build.sh
+++ deploy/build.sh
@@ -15,80 +15,84 @@ Migrator="Migrator"
 Migrator_Dockerfile_Localdir="Tasktower.Migrator/Tasktower.Migrator"
 
 Servicenames=( \
-    ${UIService} \
-    ${BoardService} \
-    ${OcelotGateway} \
-    ${SQLServerDatabase} \
-    ${Migrator})
+  ${UIService} \
+  ${BoardService} \
+  ${OcelotGateway} \
+  ${SQLServerDatabase} \
+  ${Migrator})
 
 print_help()
 {
-    echo "Usage: build.sh [OPTIONS]"
-    echo ""
-    echo "Builds docker image, if no options specified all images are build"
-    echo "Options:"
-    echo "  -h                  help"
-    echo "  -i                  list services"
-    echo "  -s service          build a specific service"
+  echo "Usage: build.sh [OPTIONS]"
+  echo ""
+  echo "Builds docker image, if no options specified all images are build"
+  echo "Options:"
+  echo "  -h                  help"
+  echo "  -i                  list services"
+  echo "  -s service          build a specific service"
 }
 
 print_service_names() {
-    echo "service information"
-    echo "-----------------------------"
-    for servicename in ${Servicenames[@]};
-    do
-        echo ${servicename}
-    done
+  echo "service information"
+  echo "-----------------------------"
+  for servicename in ${Servicenames[@]};
+  do
+    echo ${servicename}
+  done
 }
 
 build_image() {
-    servicename=$1
-    dockerfile_localdir=$2
-    tmp_startdir=$(pwd)
-    echo "----------------------------------------------------------------------"
-    echo "Building $servicename"
-    echo "----------------------------------------------------------------------"
-    cd ${dockerfile_localdir}
+  servicename=$1
+  dockerfile_localdir=$2
+  tmp_startdir=$(pwd)
+  echo "----------------------------------------------------------------------"
+  echo "Building $servicename"
+  echo "----------------------------------------------------------------------"
+  cd ${dockerfile_localdir}
 
-    base_img="tasktower-${servicename,,}"
-    tag=$(git log -1 --pretty=%H)
+  base_img="tasktower-${servicename,,}"
+  tag=$(git log -1 --pretty=%H)
 
-    img="${base_img}:${tag}"
-    latest="${base_img}:latest"
+  img="${base_img}:${tag}"
+  latest="${base_img}:latest"
 
-    docker build --no-cache -t ${img} .
-    docker tag ${img} ${latest}
+  docker build --no-cache -t ${img} .
+  docker tag ${img} ${latest}
 
-    cd ${tmp_startdir}
+  cd ${tmp_startdir}
 } 
 
 while getopts ihs: flag; do
-    case "${flag}" in
-        h)
-            print_help
-            exit 0
-        ;;
-        i)
-            print_service_names
-            exit 0
-        ;;
-        s) 
-            if [[ " ${Servicenames[@]} " =~ " $OPTARG " ]]
-            then
-                Servicenames=( "$OPTARG" )
-            else
-                echo "$OPTARG not a valid service"
-                exit -1
-            fi
-        ;;
-    esac
+  case "${flag}" in
+    h)
+      print_help
+      exit 0
+    ;;
+    i)
+      print_service_names
+      exit 0
+    ;;
+    s) 
+      if [[ " ${Servicenames[@]} " =~ " $OPTARG " ]]
+      then
+        Servicenames=( "$OPTARG" )
+      else
+        echo "$OPTARG not a valid service"
+        exit -1
+      fi
+    ;;
+    *)
+      print_help
+      exit -1
+    ;;
+  esac
 done
 
 cd ../../
 for servicename in ${Servicenames[@]};
 do
-    declare -n dockerfile_path=${servicename}_Dockerfile_Localdir
-    build_image ${servicename} $dockerfile_path
+  declare -n dockerfile_path=${servicename}_Dockerfile_Localdir
+  build_image ${servicename} $dockerfile_path
 done
 cd Tasktower.Manager/deploy
 set -- $args
--- repo-manager.sh
+++ repo-manager.sh
@@ -12,55 +12,55 @@ GIT_COMMAND=" status "
 
 help_and_exit()
 {
-    echo "Usage: gitmanager.sh [OPTIONS]"
-    echo "Options:"
-    echo "  -h                  help"
-    echo "  -c git_command      git command fot your repositories"
-    echo "                          ex) ./project-manager.sh -c \"checkout master\" "
-    echo "  -r repository       project repository e.g) ./project-manager.sh -r Tasktower.Webutils"
-    exit 0
+  echo "Usage: gitmanager.sh [OPTIONS]"
+  echo "Options:"
+  echo "  -h                  help"
+  echo "  -c git_command      git command fot your repositories"
+  echo "                          ex) ./project-manager.sh -c \"checkout master\" "
+  echo "  -r repository       project repository e.g) ./project-manager.sh -r Tasktower.Webutils"
+  exit 0
 }
 
 manageRepositories() 
 {
-    echo "__________ MANAGING REPOSITORIES __________"
+  echo "__________ MANAGING REPOSITORIES __________"
+  cd ../
+  for repo in ${repositories[@]}; do
+    echo "__________Working on ${repo}__________"
+    [ ! -d ${repo} ] && git clone https://github.com/Tasktower/${repo} 
+    cd ./${repo}
+    git ${GIT_COMMAND}   
     cd ../
-    for repo in ${repositories[@]}; do
-        echo "__________Working on ${repo}__________"
-        [ ! -d ${repo} ] && git clone https://github.com/Tasktower/${repo} 
-        cd ./${repo}
-        git ${GIT_COMMAND}   
-        cd ../
-    done
-    cd ${baseDir}
-    echo "__________ Finish working on repositories __________"
+  done
+  cd ${baseDir}
+  echo "__________ Finish working on repositories __________"
 }
 
 while getopts r:c:b:h flag; do
-    case "${flag}" in
-        h) 
-            help_and_exit
-        ;;
-        b) 
-            BRANCH=$OPTARG
-        ;;
-        c) 
-            GIT_COMMAND=$OPTARG
-        ;;
-        r)
-            if [[ " ${repositories[@]} " =~ " $OPTARG " ]]
-            then
-                echo "repository [$OPTARG] chosen"
-            else
-                echo "ERROR: repository [$OPTARG] not used"
-                exit
-            fi
-            repositories=( "$OPTARG" )
-        ;;
-        *)
-          help_and_exit
-        ;;
-    esac
+  case "${flag}" in
+    h) 
+      help_and_exit
+    ;;
+    b) 
+      BRANCH=$OPTARG
+    ;;
+    c) 
+      GIT_COMMAND=$OPTARG
+    ;;
+    r)
+      if [[ " ${repositories[@]} " =~ " $OPTARG " ]]
+      then
+        echo "repository [$OPTARG] chosen"
+      else
+        echo "ERROR: repository [$OPTARG] not used"
+        exit
+      fi
+      repositories=( "$OPTARG" )
+    ;;
+    *)
+      help_and_exit
+    ;;
+  esac
 done
 
 
