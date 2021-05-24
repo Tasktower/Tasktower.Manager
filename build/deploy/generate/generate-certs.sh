748b431a10cab895baba6d8bd8fc47a157c77169
Oren Ben-Meir <orenbm11@gmail.com>
2021-05-23 20:25:48 -0400

    added https config

---
+++ build/deploy/generate/generate-certs.sh
@@ -0,0 +1,4 @@
+﻿# generate certs
+rm ../docker/volumes/https/aspnetapp.pfx
+dotnet dev-certs https -ep ../docker/volumes/https/aspnetapp.pfx -p password
+dotnet dev-certs https -ep ../docker/volumes/https/aspnetapp.pfx --trust
