c821cbd43113e914fb38cf618fc442eea95211c3
Oren Ben-Meir <orenbm11@gmail.com>
2021-05-23 21:14:51 -0400

    added certificate support

--- build/deploy/generate/generate-certs.sh
+++ build/deploy/generate/generate-certs.sh
@@ -1,4 +1,11 @@
 ﻿# generate certs
-rm ../docker/volumes/https/aspnetapp.pfx
-dotnet dev-certs https -ep ../docker/volumes/https/aspnetapp.pfx -p password
-dotnet dev-certs https -ep ../docker/volumes/https/aspnetapp.pfx --trust
+#rm ../docker/volumes/https/Tasktower.OcelotGateway.pfx
+#dotnet dev-certs https -ep ../docker/volumes/https/Tasktower.OcelotGateway.pfx -p crypticpassword
+#dotnet dev-certs https -ep ../docker/volumes/https/Tasktower.OcelotGateway.pfx --trust
+
+rm -rf ../volumes/https
+dotnet dev-certs https -ep ${HOME}/.aspnet/https/tasktower.pfx -p crypticpassword
+dotnet dev-certs https --trust
+cd ../docker/volumes
+mkdir https
+cp ${HOME}/.aspnet/https/tasktower.pfx ./https/
